300166af35d2606273d8453f48429e0b1d0c5e4e
David Rolland <rold2007@users.noreply.github.com>
2012-07-05 14:39:33 +1100

    Clean up using keyword.

--- Program.cs
+++ Program.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Windows.Forms;
-
-namespace ImagingInterface
+﻿namespace ImagingInterface
    {
+   using System;
+   using System.Windows.Forms;
+
    static class Program
       {
       /// <summary>
