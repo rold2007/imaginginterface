d413d9f01b56bc58b27355338b63d9cf57971299
David Rolland <rold2007@yahoo.com>
2012-07-05 14:38:44 +1100

    Initial commit.

---
+++ MainWindow.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace ImagingInterface
+   {
+   partial class mainWindow
+      {
+      /// <summary>
+      /// Required designer variable.
+      /// </summary>
+      private System.ComponentModel.IContainer components = null;
+
+      /// <summary>
+      /// Clean up any resources being used.
+      /// </summary>
+      /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+      protected override void Dispose(bool disposing)
+         {
+         if (disposing && (components != null))
+            {
+            components.Dispose();
+            }
+         base.Dispose(disposing);
+         }
+
+      #region Windows Form Designer generated code
+
+      /// <summary>
+      /// Required method for Designer support - do not modify
+      /// the contents of this method with the code editor.
+      /// </summary>
+      private void InitializeComponent()
+         {
+         this.components = new System.ComponentModel.Container();
+         this.mainImageBox = new Emgu.CV.UI.ImageBox();
+         this.mainImageBoxToolTip = new System.Windows.Forms.ToolTip(this.components);
+         ((System.ComponentModel.ISupportInitialize)(this.mainImageBox)).BeginInit();
+         this.SuspendLayout();
+         // 
+         // mainImageBox
+         // 
+         this.mainImageBox.Dock = System.Windows.Forms.DockStyle.Fill;
+         this.mainImageBox.Location = new System.Drawing.Point(0, 0);
+         this.mainImageBox.Name = "mainImageBox";
+         this.mainImageBox.Size = new System.Drawing.Size(304, 266);
+         this.mainImageBox.TabIndex = 0;
+         this.mainImageBox.TabStop = false;
+         // 
+         // mainWindow
+         // 
+         this.AllowDrop = true;
+         this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+         this.ClientSize = new System.Drawing.Size(304, 266);
+         this.Controls.Add(this.mainImageBox);
+         this.Name = "mainWindow";
+         this.Text = "ImagingInterface";
+         this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+         this.DragDrop += new System.Windows.Forms.DragEventHandler(this.mainWindow_DragDrop);
+         this.DragEnter += new System.Windows.Forms.DragEventHandler(this.mainWindow_DragEnter);
+         ((System.ComponentModel.ISupportInitialize)(this.mainImageBox)).EndInit();
+         this.ResumeLayout(false);
+
+         }
+
+      #endregion
+
+      private Emgu.CV.UI.ImageBox mainImageBox;
+      private System.Windows.Forms.ToolTip mainImageBoxToolTip;
+      }
+   }
+
---
+++ MainWindow.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.UI;
+using Emgu.CV.Features2D;
+using Emgu.CV.Reflection;
+using Emgu.CV.Structure;
+using Emgu.CV.Util;
+using Emgu.Util;
+
+namespace ImagingInterface
+   {
+   public partial class mainWindow : Form
+      {
+      public mainWindow()
+         {
+         InitializeComponent();
+         }
+
+      private void mainWindow_DragDrop(object sender, DragEventArgs e)
+         {
+         if (this.DragEventValid(e))
+            {
+            string[] data = e.Data.GetData(DataFormats.FileDrop) as string[];
+
+            if (data != null)
+               {
+               string fileName = data[0];
+               Image<Rgb, UInt16> image = null;
+
+               try
+                  {
+                  image = new Image<Rgb, UInt16>(fileName);
+
+                  this.mainImageBoxToolTip.SetToolTip(this.mainImageBox, fileName);
+                  }
+               catch
+                  {
+                  this.mainImageBoxToolTip.SetToolTip(this.mainImageBox, "Invalid file format: " + fileName);
+                  }
+
+               if (this.mainImageBox.Image != null)
+                  {
+                  this.mainImageBox.Image.Dispose();
+                  }
+
+               this.mainImageBox.Image = image;
+               }
+            }
+         }
+
+      private void mainWindow_DragEnter(object sender, DragEventArgs e)
+         {
+         if (DragEventValid(e))
+            {
+            e.Effect = DragDropEffects.Copy;
+            }
+         else
+            {
+            e.Effect = DragDropEffects.None;
+            }
+         }
+
+      private bool DragEventValid(DragEventArgs e)
+         {
+         if ((e.AllowedEffect & DragDropEffects.Copy) == DragDropEffects.Copy)
+            {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+               {
+               string[] data = e.Data.GetData(DataFormats.FileDrop) as string[];
+
+               if (data.Length == 1)
+                  {
+                  return true;
+                  }
+               }
+            }
+
+         return false;
+         }
+      }
+   }
