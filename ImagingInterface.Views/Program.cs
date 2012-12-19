4d8e263c57b2dba9ef9997a08f9a39dff673b12d
David Rolland <rold2007@users.noreply.github.com>
2012-12-19 19:35:48 +1100

    Updated project structure.

--- ImagingInterface.Views/Program.cs
+++ ImagingInterface.Views/Program.cs
@@ -1,15 +1,18 @@
-﻿namespace ImagingInterface
+﻿namespace ImagingInterface.Views
    {
    using System;
+   using System.Collections.Generic;
+   using System.Linq;
+   using System.Threading.Tasks;
    using System.Windows.Forms;
 
-   public static class Program
+   static class Program
       {
       /// <summary>
       /// The main entry point for the application.
       /// </summary>
       [STAThread]
-      public static void Main()
+      static void Main()
          {
          Application.EnableVisualStyles();
          Application.SetCompatibleTextRenderingDefault(false);
