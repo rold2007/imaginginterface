83ed96e463249de21d0056c6c7b4f22b6e9e2572
David Rolland <rold2007@users.noreply.github.com>
2013-10-12 17:32:33 +1300

    Adding missing files in previous commit.

---
+++ ImagingInterface.Controllers.Interfaces/IImageViewManagerController.cs
@@ -0,0 +1,16 @@
+﻿namespace ImagingInterface.Controllers
+   {
+   using System;
+   using System.Collections.Generic;
+   using System.Linq;
+   using System.Text;
+   using System.Threading.Tasks;
+   using ImagingInterface.Controllers;
+
+   public interface IImageViewManagerController
+      {
+      void AddImage(IImageController imageController);
+      IImageController GetActiveImage();
+      void RemoveImage(IImageController imageController);
+      }
+   }
