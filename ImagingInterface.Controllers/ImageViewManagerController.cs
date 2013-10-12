b57468b4d1d290ef4d11790b370f2b17be3e8748
David Rolland <David Rolland@localhost>
2013-10-12 17:32:33 +1300

    Adding missing files in previous commit.

---
+++ ImagingInterface.Controllers/ImageViewManagerController.cs
@@ -0,0 +1,50 @@
+﻿namespace ImagingInterface.Controllers
+   {
+   using System;
+   using System.Collections.Generic;
+   using System.Linq;
+   using System.Text;
+   using System.Threading.Tasks;
+   using ImagingInterface.Views;
+   using ImagingInterface.Models;
+   using Microsoft.Practices.ServiceLocation;
+
+   public class ImageViewManagerController : IImageViewManagerController
+      {
+      private IImageViewManager imageViewManager;
+      private Dictionary<IImageView, IImageController> imageControllers;
+
+      public ImageViewManagerController(IImageViewManager imageViewManager)
+         {
+         this.imageViewManager = imageViewManager;
+         this.imageControllers = new Dictionary<IImageView, IImageController>();
+         }
+
+      public void AddImage(IImageController imageController)
+         {
+         this.imageViewManager.AddImageView(imageController.ImageView, imageController.ImageModel);
+         this.imageControllers.Add(imageController.ImageView, imageController);
+         }
+
+      public IImageController GetActiveImage()
+         {
+         IImageViewManager imageViewManager = ServiceLocator.Current.GetInstance<IImageViewManager>();
+         IImageView activeImageView = imageViewManager.GetActiveImage();
+
+         if (activeImageView != null)
+            {
+            return this.imageControllers[activeImageView];
+            }
+         else
+            {
+            return null;
+            }
+         }
+
+      public void RemoveImage(IImageController imageController)
+         {
+         this.imageViewManager.RemoveImageView(imageController.ImageView);
+         this.imageControllers.Remove(imageController.ImageView);
+         }
+      }
+   }
