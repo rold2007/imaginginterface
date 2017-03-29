58db86f48dd5d9778897a54d889a2be2b6a76de0
David Rolland <rold2007@users.noreply.github.com>
2017-03-29 16:46:35 +0900

    Added unit tests ffor ImageSourceManager.

---
+++ Core/ImagingInterface.Controllers/EventArguments/ImageSourceAddedEventArgs.cs
@@ -0,0 +1,19 @@
+﻿namespace ImagingInterface.Controllers
+{
+    using System;
+    using ImagingInterface.Plugins;
+
+    public class ImageSourceAddedEventArgs : EventArgs
+    {
+        public ImageSourceAddedEventArgs(IImageSource imageSource)
+        {
+            this.ImageSource = imageSource;
+        }
+
+        public IImageSource ImageSource
+        {
+            get;
+            private set;
+        }
+    }
+}
---
+++ Core/ImagingInterface.Controllers/EventArguments/ImageSourceRemovedEventArgs.cs
@@ -0,0 +1,19 @@
+﻿namespace ImagingInterface.Controllers
+{
+    using System;
+    using ImagingInterface.Plugins;
+
+    public class ImageSourceRemovedEventArgs : EventArgs
+    {
+        public ImageSourceRemovedEventArgs(IImageSource imageSource)
+        {
+            this.ImageSource = imageSource;
+        }
+
+        public IImageSource ImageSource
+        {
+            get;
+            private set;
+        }
+    }
+}
