f5e7c7f59a41221662625e42f7dfd0eb0e3e5621
David Rolland <rold2007@yahoo.com>
2017-04-05 15:47:13 +0900

    Corrected code coverage for AboutBox.

--- Core/ImagingInterface.Controllers/AboutBoxController.cs
+++ Core/ImagingInterface.Controllers/AboutBoxController.cs
@@ -1,18 +1,18 @@
 ﻿namespace ImagingInterface.Controllers
-   {
+{
    using ImagingInterface.Models;
 
    public class AboutBoxController
-      {
+   {
       public AboutBoxController(AboutBoxModel aboutBoxModel)
-         {
+      {
          this.AboutBoxModel = aboutBoxModel;
-         }
+      }
 
       public IAboutBoxModel AboutBoxModel
-         {
+      {
          get;
          private set;
-         }
       }
    }
+}
