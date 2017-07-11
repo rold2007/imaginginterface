d4cb27983aa1d2a92e3e634fec6650eedab16f0d
David Rolland <rold2007@yahoo.com>
2017-07-11 16:46:33 +0900

    Extracted code from AboutBox to an external service.

--- Core/Tests/ImagingInterface.Controllers.Tests/AboutBoxControllerTest.cs
+++ Core/Tests/ImagingInterface.Controllers.Tests/AboutBoxControllerTest.cs
@@ -1,21 +1,21 @@
 ﻿namespace ImagingInterface.Controllers.Tests
 {
-    using ImagingInterface.Models;
-    using NUnit.Framework;
+   using ImagingInterface.Controllers.Services;
+   using NUnit.Framework;
 
-    [TestFixture]
-    public class AboutBoxControllerTest : ControllersBaseTest
-    {
-        [Test]
-        public void Constructor()
-        {
-            AboutBoxModel aboutBoxModel = new AboutBoxModel();
-            AboutBoxController aboutBoxController = new AboutBoxController(aboutBoxModel);
+   [TestFixture]
+   public class AboutBoxControllerTest : ControllersBaseTest
+   {
+      [Test]
+      public void Constructor()
+      {
+         ApplicationPropertiesService applicationPropertiesService = new ApplicationPropertiesService();
+         AboutBoxController aboutBoxController = new AboutBoxController(applicationPropertiesService);
 
-            Assert.That(aboutBoxController.Product.ProductName, Is.Not.Null.Or.Empty);
-            Assert.That(aboutBoxController.Product.Version, Is.Not.Null.Or.Empty);
-            Assert.That(aboutBoxController.Product.Copyright, Is.Not.Null.Or.Empty);
-            Assert.That(aboutBoxController.Product.ProductDescription, Is.Not.Null.Or.Empty);
-        }
-    }
+         Assert.That(aboutBoxController.Product.ProductName, Is.Not.Null.Or.Empty);
+         Assert.That(aboutBoxController.Product.Version, Is.Not.Null.Or.Empty);
+         Assert.That(aboutBoxController.Product.Copyright, Is.Not.Null.Or.Empty);
+         Assert.That(aboutBoxController.Product.ProductDescription, Is.Not.Null.Or.Empty);
+      }
+   }
 }
